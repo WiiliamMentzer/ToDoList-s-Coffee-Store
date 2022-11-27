d29f0219c6468a6dd359121ceb6a71513e99d50c
WiiliamMentzer <william.mentzer2k@gmail.com>
2022-11-27 05:06:01 -0800

    added CoffeeForm.js and began scafolding for CoffeeControl.js - WIP

---
+++ src/components/CoffeeControl.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Coffee from "./Coffee";
+
+class CoffeeControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  render(){
+
+    <Coffee />
+  }
+
+}
+
+export default CoffeeControl;
